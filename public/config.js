d3ffccc54d515a15f839664e3f620a59bb2f9d75
Catalin Ciocov <catalin.ciocov@gmail.com>
2019-02-21 13:49:27 +0200

    Added field formatters for campaigns and line items.

--- public/config.js
+++ public/config.js
@@ -3,6 +3,14 @@ export default {
     section: {
       title: 'Section',
       fieldType: ['number', 'string']
+    },
+    campaign: {
+      title: 'Campaign',
+      fieldType: ['number', 'string']
+    },
+    lineitem: {
+      title: 'Line Item',
+      fieldType: ['number', 'string']
     }
   }
 }
--- server/mappings.js
+++ server/mappings.js
@@ -10,6 +10,16 @@ const queries = {
     SELECT Section_ID AS id, Section_Name AS name
     FROM \`ism-data.raw_isap_metadata.sections_and_publishers_raw\`
     GROUP BY Section_ID, Section_Name
+  `,
+  campaign: `
+    SELECT Campaign_ID AS id, Campaign_Name AS name
+    FROM \`ism-data.raw_isap_metadata.campaigns_raw\`
+    GROUP BY Campaign_ID, Campaign_Name
+  `,
+  lineitem: `
+    SELECT Line_Item_ID AS id, Line_Item_Name AS name
+    FROM \`ism-data.raw_isap_metadata.line_items_raw\`
+    GROUP BY Line_Item_ID, Line_Item_Name
   `
 };
 
@@ -26,7 +36,13 @@ export async function init(server) {
     }
   });
 
+  // load mappings:
   Object.keys(config.mappings).forEach(mappingId => loadMapping(mappingId));
+
+  // refresh mappings every day:
+  setInterval(function() {
+    Object.keys(config.mappings).forEach(mappingId => loadMapping(mappingId));
+  }, 86400 * 1000);
 }
 
 export async function loadMapping(mappingId) {
@@ -47,4 +63,6 @@ export async function loadMapping(mappingId) {
   for (let i = 0; i < rows.length; i++) {
     mappings[mappingId].data[rows[i].id] = rows[i].name;
   }
+
+  console.log(JSON.stringify(mappings[mappingId], null, 2));
 }
